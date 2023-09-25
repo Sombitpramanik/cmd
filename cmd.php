8f3d8dc30c722f311b88ff08477e6ca5191b4ff8
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-25 19:11:06 +0530

    first commit

--- cmd.php
+++ cmd.php
@@ -2,15 +2,14 @@
 $username = "phpuser";
 $password = "php123";
 
-// Use the exec() function to run the useradd command
+// Use the exec() function to run the useradd command with sudo
 $command = "sudo useradd -m $username";
 exec($command, $output, $returnCode);
 
-// Check the return code to see if the user was added successfully
 if ($returnCode === 0) {
     // User added successfully, now set the password
     $passwordCommand = "sudo passwd $username";
-    exec("$passwordCommand <<< '$password\n$password\n'", $passwordOutput, $passwordReturnCode);
+    exec("echo '$password\n$password\n' | $passwordCommand", $passwordOutput, $passwordReturnCode);
 
     if ($passwordReturnCode === 0) {
         echo "User $username created and password set.";
